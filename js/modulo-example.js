cca49fdf2b452be1f3082bf461c6672821111153
musicfreakt <yaloveganikita@outlook.com>
2020-05-26 22:31:49 +0300

    Add some theory

---
+++ js/modulo-example.js
@@ -0,0 +1,6 @@
+function modulo_example()
+{
+    var a = document.getElementById('c1').value;
+    var b = document.getElementById('c2').value;
+    document.getElementById('ans').innerHTML = a%b;
+}
